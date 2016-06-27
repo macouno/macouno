25de2fefa4cf9de9138996328848e171851c29ff
macouno <dolf@macouno.com>
2016-06-27 21:55:28 +0200

    Made surface net more consistens... now the startup script should have the functions and only add the SNet_enabled variable. The rest of the vars should be added to surface net objects only.

--- Blender/startup/surface_net.py
+++ Blender/startup/surface_net.py
@@ -4,19 +4,19 @@ from bpy.props import EnumProperty, IntProperty
 
 	
 @persistent
-def SurfaceNetUpdate(context):
+def SNet_Update(context):
 	
 	#scn = context.scene
 	
 	for ob in context.objects:
-		if ob.sn_enabled:
+		if ob.SNet_enabled:
 			ob.location[0] += 0.01
 
 
 	
 	
 # Add a charm panel to every object
-class Object_surface_net(bpy.types.Panel):
+class Object_SNet(bpy.types.Panel):
 	bl_label = "Surface Net"
 	bl_space_type = "PROPERTIES"
 	bl_region_type = "WINDOW"
@@ -29,20 +29,12 @@ class Object_surface_net(bpy.types.Panel):
 		ob = context.object
 		
 		row = layout.row()
-		row.prop(ob,'sn_enabled')
-		
-		#These should only be visible whilst debugging really
-		
-		row = layout.row()
-		row.prop(ob,'sn_growthRate')
-		
-		row = layout.row()
-		row.prop(ob,'sn_gridSize')
+		row.prop(ob,'SNet_enabled')
 		
 		
 		
-def set_net(self, value):
-	if self.sn_enabled:
+def SNet_Set(self, value):
+	if self.SNet_enabled:
 		print('Enabling Surface Net')
 	else:
 		print('Disabling Surface Net')
@@ -52,29 +44,21 @@ def set_net(self, value):
 def register():
 	
 	# Is this object a net?
-	bpy.types.Object.sn_enabled = bpy.props.BoolProperty(default=False, name="Enable Surface Net", update=set_net)
+	bpy.types.Object.SNet_enabled = bpy.props.BoolProperty(default=False, name="Enable Surface Net", update=SNet_Set)
 	
-	bpy.types.Object.sn_growthRate = bpy.props.FloatProperty(name="Growth Rate", description="", default=0.05, min=sys.float_info.min, max=sys.float_info.max, soft_min=sys.float_info.min, soft_max=sys.float_info.max, step=3, precision=2, options={'ANIMATABLE'}, subtype='NONE', unit='NONE', update=None, get=None, set=None)
-	
-	#bpy.types.Object.sn_growthRate = bpy.props.IntProperty(name="Growth Rate", description="", default=0, min=-2**31, max=2**31-1, soft_min=-2**31, soft_max=2**31-1, step=1, options={'ANIMATABLE'}, subtype='NONE', update=None, get=None, set=None)
-	
-	bpy.types.Object.sn_gridSize = bpy.props.IntVectorProperty(name="Grid Size", description="", default=(10, 10, 10), min=-2**31, max=2**31-1, soft_min=-2**31, soft_max=2**31-1, step=1, options={'ANIMATABLE'}, subtype='NONE', size=3, update=None, get=None, set=None)
-	
-	bpy.utils.register_class(Object_surface_net)
+	bpy.utils.register_class(Object_SNet)
 	#bpy.app.handlers.frame_change_pre.append(SurfaceNetUpdate)
-	bpy.app.handlers.scene_update_pre.append(SurfaceNetUpdate)
+	bpy.app.handlers.scene_update_pre.append(SNet_Update)
 
 
 	
 def unregister():
 	
-	del bpy.types.Object.sn_enabled
-	del bpy.types.Object.sn_growthRate
-	del bpy.types.Object.sn_gridSize
+	del bpy.types.Object.SNet_enabled
 
-	bpy.utils.unregister_class(Object_surface_net)
+	bpy.utils.unregister_class(Object_SNet)
 	#bpy.app.handlers.frame_change_pre.remove(SurfaceNetUpdate)
-	bpy.app.handlers.scene_update_pre.remove(SurfaceNetUpdate)
+	bpy.app.handlers.scene_update_pre.remove(SNet_Update)
 
 
 
